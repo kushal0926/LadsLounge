c1624ce8a1a4fbf8bf0a7f25886be6867a243b28
kushal_tamang <kushaltamang0926@gmail.com>
2024-09-04 16:36:16 +0545

    all change

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,12 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
+
+// booking site open when i click the button
 function navigateToPage() {
     window.location.href = "booking.html";
 }
+
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+})
